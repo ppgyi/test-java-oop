1c25f1dde47c51b36589c938509ad25d09bf2994
Peter <peter.palagyi@gmail.com>
2022-02-22 12:32:57 +0100

    Variables

--- src/main/java/sk/pgyi/oop/App.java
+++ src/main/java/sk/pgyi/oop/App.java
@@ -3,5 +3,24 @@ package sk.pgyi.oop;
 public class App {
     public static void main(String[] args){
         System.out.println("Ahooj svet");
+
+//        primitivne datove typy
+
+        int celeCislo = 5;
+        long celeCisloVacsie = 5L;
+        float desatinneCisloSinglePrecision = 5.5f;
+        double desatinneCisloDoublePrecision = 5.5;
+        char znak = 'a';
+        boolean pravdaNepravda = true; // false
+
+//        objekt - OOP
+
+        String retazec = "a";
+
+//        plus minus ... funguju normalne, ale len rovnake typy
+
+        int podiel = 2/3; // 0
+        int poDeleni = 2%3; // 2
+        float podielDesatiny = 2/3.0f; // 0.6666667
     }
 }
