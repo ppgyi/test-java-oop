5fd2bfa4ac0ccff01dd7ab941765ca1a5d554e48
Peter <peter.palagyi@gmail.com>
2022-02-23 09:17:56 +0100

    boolean, konstruktor, metoda namiesto settra

--- src/main/java/sk/pgyi/oop/Auto.java
+++ src/main/java/sk/pgyi/oop/Auto.java
@@ -6,14 +6,22 @@ public class Auto {
     private double kapacitaNadrze;
     private double stavNadrze;
     public String nazov;
+    private boolean neojazdene; // Defaultna hodnota je false
 
     private static final double FUEL_PER_KM = 0.5;
 
     // Ukazka moznosti podla stupna verejnosti, od public po private
+
     public int one; // viem sa dostat stale
     int two; // neviem sa dostat pokial je class v inom package
     protected int three; //nieco s dedenim
     private int four; // neviem sa dostat
+
+    // Konstruktor vytvara instancie a zadava prvotne hodnoty
+    public Auto() { // Je to bezparametricky konstruktor, moze byt aj private
+        neojazdene = true; // Tu viem zmenit defaulnu hodnotu boolean na true
+        kapacitaNadrze = 100;
+    }
 
     public void drive(double distanceInKm) {
         stavNadrze -= distanceInKm * FUEL_PER_KM;
@@ -24,6 +32,11 @@ public class Auto {
 
     public double getStavNadrze() {
         return stavNadrze;
+    }
+
+    // Namiesto settra mozem urobit nejaku metodu ktora nasetuje stavNadrze:
+    public void dotankovat(){
+        stavNadrze = kapacitaNadrze;
     }
 
     public void setStavNadrze(double stavNadrze) {
