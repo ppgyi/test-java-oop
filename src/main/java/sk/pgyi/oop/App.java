a46e31c88fb77f14789e911e1ec319fec0947285
Peter <peter.palagyi@gmail.com>
2022-02-25 19:19:05 +0100

    HRA MINY - trivialna

--- src/main/java/sk/pgyi/oop/App.java
+++ src/main/java/sk/pgyi/oop/App.java
@@ -1,24 +1,10 @@
 package sk.pgyi.oop;
 
-import java.util.*;
+import sk.pgyi.oop.game.Game;
 
 public class App {
     public static void main(String[] args){
-        List<Animal> animals = new ArrayList<>(List.of(new Dog("Rex"),
-                new Dog("Luna"), new Bird("Polly"),
-                new Bird("Orol")));
-
-        // LAMBDA - anonymna f-cia, cez sipku, teraz nemusim mat
-        // AnimalComparator tiedu! Staci takto:
-        animals.sort((o1, o2) -> {
-            return o1.toString().compareTo(o2.toString());
-        });
-        // Jednoduchy zapis:
-        animals.sort((o1, o2) -> o1.toString().compareTo(o2.toString()));
-
-        // Preddefinovana metoda: (to iste ako prve a druhe)
-        animals.sort(Comparator.comparing(Animal::toString));
-
-        System.out.println(animals.toString());
+        Game g = new Game();
+        g.play();
     }
 }
---
+++ src/main/java/sk/pgyi/oop/game/tiles/MineRevealedException.java
@@ -0,0 +1,4 @@
+package sk.pgyi.oop.game.tiles;
+
+public class MineRevealedException extends Exception {
+}
