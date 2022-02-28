8313f39d31c876da85b1800a36bf5fb5ca0d1292
Peter <peter.palagyi@gmail.com>
2022-02-28 13:18:30 +0100

    continue, break s label

--- src/main/java/sk/pgyi/oop/App.java
+++ src/main/java/sk/pgyi/oop/App.java
@@ -3,6 +3,17 @@ package sk.pgyi.oop;
 public class App {
     public static void main(String[] args){
 
-        new Okno();
+        // break a continue s labelom, presunie na vonkajsi cyklus:
+        vonkajsiFor:
+        for (int i = 0; i < 100; i++){
+            for (int j = 10; j > 0; j--){
+                System.out.println("---");
+                if (i%4 == 0){
+                    continue vonkajsiFor;
+                }
+                System.out.println(i + ", " + j);
+            }
+        }
+        System.out.println("koniec");
     }
 }
